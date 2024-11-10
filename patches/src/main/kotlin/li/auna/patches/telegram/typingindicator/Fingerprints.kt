197f9ee4e046135a2b298861485d57b11a405099
Aunali321 <aunvakil.aa@gmail.com>
2024-11-11 00:51:18 +0530

    fix(telegram): Find correct method

--- patches/src/main/kotlin/li/auna/patches/telegram/typingindicator/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/typingindicator/Fingerprints.kt
@@ -1,11 +1,9 @@
 package li.auna.patches.telegram.typingindicator
 
 import app.revanced.patcher.fingerprint
-import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
 internal val needSendTypingFingerprint = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.CONSTRUCTOR)
     returns("V")
     custom { methodDef, _ ->
         methodDef.name == "needSendTyping"
