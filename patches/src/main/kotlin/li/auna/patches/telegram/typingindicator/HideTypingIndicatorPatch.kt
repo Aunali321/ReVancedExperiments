74f77adfd6b67605dc082292b7b77b29d093dc83
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2024-12-07 16:46:34 +0530

    support Cherrygram

--- patches/src/main/kotlin/li/auna/patches/telegram/typingindicator/HideTypingIndicatorPatch.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/typingindicator/HideTypingIndicatorPatch.kt
@@ -11,6 +11,7 @@ val hideAdsPatch = bytecodePatch(
     compatibleWith(
         "org.telegram.messenger",
         "org.telegram.messenger.web"
+        "uz.unnarsx.cherrygram"
     )
 
     execute {
